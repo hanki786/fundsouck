b99b65e002f191c0313001d470aaf6dd0a680cd8
Ahsan Shaokat <ahsan.shaokat@purelogics.net>
2017-12-27 20:19:11 +0500

    dropdown change

--- resources/views/website/fund_overview.blade.php
+++ resources/views/website/fund_overview.blade.php
@@ -14,14 +14,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-            <h3>Fund Overview</h3>
-            <span class="pull-right">
-                <select class="form-control" name="fund_identity_id">
-                    @foreach($fund_identities as $fund_identity)
-                        <option value="{{ $fund_identity->id }}">{{ $fund_identity->fund_name }}</option>
-                    @endforeach
-                </select>
-            </span>
+            <h3>Fund Overview
+                <select class="pull-right" name="fund_identity_id">
+                        @foreach($fund_identities as $fund_identity)
+                            <option value="{{ $fund_identity->id }}">{{ $fund_identity->fund_name }}</option>
+                        @endforeach
+                    </select>
+            </h3>
             <div class="panel-group">
                     <div class="panel panel-default">
                         <div class="panel-heading">
