6ed5e8f5c126ac098f9ec611d7c501c046aae1e7
Ahsan Shaokat <ahsan.shaokat@purelogics.net>
2017-12-27 19:47:40 +0500

    header changes

--- resources/views/layout/header_script.blade.php
+++ resources/views/layout/header_script.blade.php
@@ -20,3 +20,35 @@
 <link href="{{ asset('vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ asset('vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<style>
+.btn-primary {
+    color: #fff;
+    background-color: #2e1d3e;
+    border-color: #2e1d3e;
+}
+.btn-primary:hover {
+    color: #fff;
+    background-color: #4e3365;
+    border-color: #3f2554;
+}
+.header-fixed .app-header {
+    position: fixed;
+    z-index: 1020;
+    width: 100%;
+    background-color: #e2e2e2;
+}
+.app-header.navbar .navbar-brand {
+    display: inline-block;
+    width: 155px;
+    height: 55px;
+    padding: 1.25rem 1rem;
+    margin-right: 0;
+    /* background-color: #fff; */
+    background-image: url(../img/logo.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 70px auto;
+    border-bottom: 1px solid #c2cfd6;
+}
+</style>
