903dc0317aa15d9ff4382b88585d20790ea85c3a
Ahsan Shaokat <ahsan.shaokat@purelogics.net>
2017-12-27 20:56:33 +0500

    text changes 2

--- resources/views/website/fund_overview.blade.php
+++ resources/views/website/fund_overview.blade.php
@@ -76,10 +76,10 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" href="#1">Latest Price</a>
+                                <a data-toggle="collapse" href="#5">Latest Price</a>
                             </h4>
                         </div>
-                        <div id="1" class="panel-collapse collapse in">
+                        <div id="5" class="panel-collapse collapse in">
                             <div class="panel-body">
                                 Body 1
                             </div>
@@ -89,10 +89,10 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" href="#1">YTD Performance</a>
+                                <a data-toggle="collapse" href="#6">YTD Performance</a>
                             </h4>
                         </div>
-                        <div id="1" class="panel-collapse collapse in">
+                        <div id="6" class="panel-collapse collapse in">
                             <div class="panel-body">
                                 Body 1
                             </div>
@@ -102,10 +102,10 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse in" href="#2">Fund Key Attributes</a>
+                                <a data-toggle="collapse in" href="#7">Fund Key Attributes</a>
                             </h4>
                         </div>
-                        <div id="2" class="panel-collapse collapse in">
+                        <div id="7" class="panel-collapse collapse in">
                             <div class="panel-body">
                                 Body 2
                             </div>
@@ -115,10 +115,10 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" href="#3">Fund Sales & Distribution</a>
+                                <a data-toggle="collapse" href="#8">Fund Sales & Distribution</a>
                             </h4>
                         </div>
-                        <div id="3" class="panel-collapse collapse">
+                        <div id="8" class="panel-collapse collapse in">
                             <div class="panel-body">
                                 Body 3
                             </div>
@@ -128,10 +128,10 @@
                     <div class="panel panel-default col-md-6">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" href="#4">Subscription</a>
+                                <a data-toggle="collapse" href="#9">Subscription</a>
                             </h4>
                         </div>
-                        <div id="4" class="panel-collapse collapse">
+                        <div id="9" class="panel-collapse collapse in">
                             <div class="panel-body">
                                 Body 4
                             </div>
@@ -141,10 +141,10 @@
                     <div class="panel panel-default col-md-6">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" href="#4">Subscription</a>
+                                <a data-toggle="collapse" href="#11">Redemption</a>
                             </h4>
                         </div>
-                        <div id="4" class="panel-collapse collapse">
+                        <div id="11" class="panel-collapse collapse">
                             <div class="panel-body">
                                 Body 4
                             </div>
