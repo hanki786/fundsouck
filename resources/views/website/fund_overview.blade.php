985da323eaae4e88533fd3e1ad55f5f2fb5b1585
Ahsan Shaokat <ahsan.shaokat@purelogics.net>
2017-12-27 20:03:18 +0500

    Text Change 1

--- resources/views/website/fund_overview.blade.php
+++ resources/views/website/fund_overview.blade.php
@@ -1,23 +1,11 @@
 @extends('layout.header')
 @section('title')
-    Profile - View
+    Fund Overview
 @endsection
 @section('breadcrumb')
     <!-- Breadcrumb -->
     <ol class="breadcrumb">
-        <li class="breadcrumb-item">Homess</li>
-        <li class="breadcrumb-item">Company Shells</li>
-        <li class="breadcrumb-item active">Company Information</li>
-        <li class="breadcrumb-item"><a  href="{{ route('list-profile-on-admin-side') }}">Profile - List</a></li>
-        <li class="breadcrumb-item active">Profile - View</li>
-        <!-- Breadcrumb Menu-->
-        <li class="breadcrumb-menu d-md-down-none">
-            <div class="btn-group" role="group" aria-label="Button group">
-                <a class="btn" href="#"><i class="icon-speech"></i></a>
-                <a class="btn" href="{{ route('dashboard') }}"><i class="icon-graph"></i> &nbsp;Dashboard</a>
-                <a class="btn" href="#"><i class="icon-settings"></i> &nbsp;Settings</a>
-            </div>
-        </li>
+        <li class="breadcrumb-item">Home</li>
     </ol>
 @endsection
 @section('content')
