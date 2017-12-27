96c186dd2b1efc2456e7e762c7d8627ec4acb0a7
Ahsan Shaokat <ahsan.shaokat@purelogics.net>
2017-12-27 20:30:14 +0500

    layout chnages

--- resources/views/website/fund_overview.blade.php
+++ resources/views/website/fund_overview.blade.php
@@ -16,7 +16,7 @@
             <div class="col-md-12">
             <h3>Fund Overview
             </h3>
-            <div class="panel-group">
+            <div class="panel-group col-md-7">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
@@ -70,6 +70,77 @@
                     </div>
 
                 </div>
+
+
+                <div class="panel-group col-md-7">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" href="#1">Latest Price</a>
+                            </h4>
+                        </div>
+                        <div id="1" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                Body 1
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" href="#1">YTD Performance</a>
+                            </h4>
+                        </div>
+                        <div id="1" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                Body 1
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" href="#2">Fund Objective & Strategy</a>
+                            </h4>
+                        </div>
+                        <div id="2" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Body 2
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" href="#3">Fund Asset Attributes</a>
+                            </h4>
+                        </div>
+                        <div id="3" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Body 3
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" href="#4">Fund Classification & Benchmarks</a>
+                            </h4>
+                        </div>
+                        <div id="4" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Body 4
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                
             </div>
         </div>
     </div>
