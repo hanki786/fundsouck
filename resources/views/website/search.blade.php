408b42263a0725a4fbd1c10e2d3579c3731a12f9
hanki786 <hanki.chanki786@gmail.com>
2018-05-26 22:55:41 +0500

    deb

--- resources/views/website/search.blade.php
+++ resources/views/website/search.blade.php
@@ -2,5 +2,5 @@
 @foreach($fi as $fund_identity)
         <h4><a href="{{ route('fund_overview_result') }}?fund_identity_id={{ $fund_identity->id }}" title="">{{ $fund_identity->fund_name }}</a></h4>
         <p>{{ $fund_identity->fund_souk_code }} - {{ $fund_identity->fund_short_name }}</p>
-        </hr>
+        <hr>
 @endforeach
