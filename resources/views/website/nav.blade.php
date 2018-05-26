55bdc8da8224156ccf9ec37704e537d0178dd1f2
hanki786 <hanki.chanki786@gmail.com>
2018-05-27 00:30:07 +0500

    rash

--- resources/views/website/nav.blade.php
+++ resources/views/website/nav.blade.php
@@ -40,7 +40,7 @@
         <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1" style="    margin-top: 10px;">
             <nav class="menu menu--iris">
                 <ul class="nav navbar-nav menu__list">
-                    <li class="menu__item menu__item--current"><a href="/" class="menu__link">About</a></li>
+                    <li class="menu__item"><a href="/" class="menu__link">About</a></li>
                     <li class="menu__item"><a href="#cc" data-toggle="modal" class="menu__link">News & Events</a></li>
                     <li class="menu__item"><a href="#contact" data-toggle="modal" class="menu__link">Contact</a></li>
                     <li class="menu__item"><a href="#about" data-toggle="modal" class="menu__link">Careers</a></li>
